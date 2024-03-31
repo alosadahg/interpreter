fcd06f58fd0673a381e7277d90c14b91801588fe
hernah <hgalosada@gmail.com>
2024-03-31 13:53:37 +0800

    Expr.java is the output of GenerateAst.java

--- interpreter/GenerateAst.java
+++ interpreter/GenerateAst.java
@@ -26,7 +26,7 @@ public class GenerateAst {
         String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.craftinginterpreters.lox;");
+        writer.println("package interpreter;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
